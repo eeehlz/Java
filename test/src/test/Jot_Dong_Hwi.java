5e2492ffa1c45060314cff05c3ce6151fe51de0a
LEE HYEOKJIN <bbq9375@naver.com>
2023-07-27 21:11:13 +0900

    장난입니다..

--- test/src/test/Jot_Dong_Hwi.java
+++ test/src/test/Jot_Dong_Hwi.java
@@ -3,8 +3,12 @@ package test;
 public class Jot_Dong_Hwi {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("예린 ❤️ 동휘 ❤️ 민향");
+		String man = "동휘";
+		String girl01 = "민향";
+		String girl02 = "예린";
+		
+		System.out.print(man + " 는 " + girl01 +"과 의 연애는 실패 하지만..." + "서울에서 " + girl02 + "와 살인적인 연애 후 결별..");
+		
 
 	}
 
